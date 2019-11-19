2bdce062c757fe122dd42579dd89df4087a3152f
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-19 16:01:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,31 +43,18 @@ def total_gross(source)
   row_index = 0 
   grand_gross = 0  
 while row_index < source.length do 
-  # grand_gross[list_of_directors] = 0 
   movies = source[row_index][:movies]
+ 
   element_index = 0 
   while element_index < movies.length do 
+  grand_gross += movies[element_index][:worldwide_gross]
     
-p grand_gross += movies[element_index][:worldwide_gross]
-  
-  
   element_index += 1 
 end
 
 row_index += 1 
 end
  p grand_gross
-# binding.pry 
 end
 
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
-
-
+ 
